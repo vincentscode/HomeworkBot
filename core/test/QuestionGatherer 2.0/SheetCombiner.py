b44acb70801595690edb74d4f3567ff78e297566
VincentsCode <vincent.schmandt@gmx.de>
2018-05-26 23:01:17 +0200

    Updated

--- core/test/QuestionGatherer 2.0/SheetCombiner.py
+++ core/test/QuestionGatherer 2.0/SheetCombiner.py
@@ -1,20 +1,43 @@
 import os
+import numpy as np
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
 
+task_ctr = []
+
+all_tasks = []
 
 full_str = ""
-for ctr in range(134, len(os.listdir("C:/Users/vince/Desktop/book5_t/"))+1):
+for ctr in range(1, len(os.listdir("C:/Users/vince/Desktop/book5_t/"))+1):
     f_name = 'C:\\Users\\vince\\Desktop\\book5_t\\{}.txt'.format(ctr)
     print(f_name)
     f = open(f_name, "r")
     m_str = f.read()
-    m_str = m_str.split("\n")
-    m_str2 = ""
-    for i in range(0, len(m_str), 2):
-        m_str2 += m_str[i]
 
-    full_str += "".join(m_str)
+    m_tasks = m_str.split("\n\n\n\n")
+    m_tasks = m_tasks[0:-1]
+    task_ctr.append(len(m_tasks))
+
+    for tsk in m_tasks:
+        all_tasks.append(m_tasks)
+
+    full_str += m_str
     f.close()
 
-f = open("C:\\Users\\vince\\Desktop\\book5_full.txt", "w")
-f.write(full_str)
-f.close()
+n_tasks = []
+for tsk in all_tasks:
+    for t in tsk:
+        t = t.replace("\n", "")
+        n_tasks.append(t)
+
+for i in range(1, len(n_tasks)):
+    c_a = 'A{}'.format(i)
+    c_b = 'B{}'.format(i)
+    ws[c_a] = str(i)
+    ws[c_b] = n_tasks[i]
+
+wb.save("C:/Users/vince/Desktop/output5.xlsx")
+
+print("Avg tasks per page:", np.mean(task_ctr))
+print("Task_count:", len(n_tasks))
