2df4cafeca01e14ef0a9914b46155dd420fad37c
VincentsCode <vincent.schmandt@gmx.de>
2018-08-10 20:55:04 +0200

    cleaned

--- core/QuestionLoader/ExcelReader.py
+++
@@ -1,7 +0,0 @@
-import openpyxl
-wb = openpyxl.load_workbook('C:\\Users\\vince\\Desktop\\output5.xlsx')
-ws = wb[wb.sheetnames[0]]
-
-tasks = [ws['B{}'.format(i)].value for i in range(1, 4000) if ws['B{}'.format(i)].value is not None]
-print(tasks)
-print(len(tasks))
